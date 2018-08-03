536532098da4b8c448785da40cc30ac843549d38
Manuel Tuero <manutuero@gmail.com>
2018-08-02 22:37:36 -0300

    I add a string_utils package and a functional-oriented solution for the isPrime function from MathOperation class

---
+++ src/string_utils/Strings.java
@@ -0,0 +1,33 @@
+package string_utils;
+
+public class Strings {
+    public boolean isCapicua(String s) {
+
+        boolean isCapicua = true;
+        int i = 0;
+        int j = s.length() - 1;
+        char inc, dec;
+
+        while (i < j) {
+            inc = s.charAt(i);
+            dec = s.charAt(j);
+
+            if(inc == ' ') {
+                i++;
+                continue;
+            } else if(dec == ' ') {
+                j--;
+                continue;
+            }
+
+            if(s.charAt(i) != s.charAt(j)) {
+                   isCapicua = false;
+                   break;
+            }
+
+            i++; j--;
+        }
+
+        return isCapicua;
+    }
+}
---
+++ src/string_utils/StringsRefactored.java
@@ -0,0 +1,5 @@
+package string_utils;
+
+public class StringsRefactored {
+
+}
